596cdb5c84354e4c4b6af62e0c07699fe45742a6
waqar <wakku9780@gmail.com>
2024-05-23 16:22:38 +0530

    Add project files.

---
+++ Session/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using Session.Models;
+using System.Diagnostics;
+
+namespace Session.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("MyKey", "Programentor");
+            TempData["SessionID"] = HttpContext.Session.Id;
+            return View();
+        }
+        public IActionResult About()
+        {
+            if (HttpContext.Session.GetString("MyKey") != null)
+            {
+                ViewBag.Data = HttpContext.Session.GetString("MyKey").ToString();
+            }
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            if (HttpContext.Session.GetString("MyKey") != null)
+            {
+                 HttpContext.Session.Remove("MyKey");
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
